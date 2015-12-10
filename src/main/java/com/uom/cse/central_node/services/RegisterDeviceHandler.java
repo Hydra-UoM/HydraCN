556ca3c9df2803ceb000e3c6edaa160a60f7bd7d
Sirojan Tharmakulasingam <sirojan.11@cse.mrt.ac.lk>
2015-12-10 16:22:55 +0530

    CEP event feeder implemented

--- src/main/java/com/uom/cse/central_node/services/RegisterDeviceHandler.java
+++ src/main/java/com/uom/cse/central_node/services/RegisterDeviceHandler.java
@@ -14,6 +14,7 @@ import com.uom.cse.central_node.event.ApplicationEvent;
 import com.uom.cse.central_node.handler.ProcessInfoEventHandler;
 import com.uom.cse.central_node.model.Device;
 import com.uom.cse.central_node.subscriber.CriticalEventSubscriber;
+import com.uom.cse.central_node.util.EventFeeder;
 import com.uom.cse.central_node.view.DeviceOverviewController;
 
 import javafx.application.Application;
@@ -56,42 +57,17 @@ public class RegisterDeviceHandler implements RegisterDeviceService.Iface {
 	@Override
 	public void pushProcessesInfo(List<ThriftAgentProcessInfo> processes) throws TException {
 		// TODO Auto-generated method stub
-		Filter customFilter = new Filter();
-		customFilter.setCpuUsage(44);
-		customFilter.setRamUsage(42);
-		customFilter.setSentData(450);
-		customFilter.setReceivedData(450);
-		customFilter.setFilterName("dummyFilter");
-		customFilter.setEventId(1000);
-		processInfoEventHandler = new ProcessInfoEventHandler(new CriticalEventSubscriber(customFilter));
-		
-		ExecutorService xrayExecutor = Executors.newSingleThreadExecutor();
-	    xrayExecutor.submit(new Runnable() {
-	            public void run() {
-	            	
-	            	int count = 0;
-	            	
-	            	while(count < 100){
-	        			
-	        			ApplicationEvent event = new ApplicationEvent(new ThriftAgentProcessInfo("name", "packageName", new Random().nextInt(50), new Random().nextInt(50), new Random().nextInt(2000), new Random().nextInt(2000), "15", "Android", "123:33:45:333"));
-	            		//ApplicationEvent event = new ApplicationEvent("name","packagename",new Random().nextInt(50),new Random().nextInt(50),new Random().nextInt(500),new Random().nextInt(500),"100","Android","1002:4444:456:78");
-	                     //   sampleEventHandler.handle(ve);
-	            		processInfoEventHandler.handle(event);
-	        			//System.out.println("testing");
-	        		}
+		int count = 0;
+    	
+    	while(count < 100){
+			
+    		processes.add(new ThriftAgentProcessInfo("name", "packageName", new Random().nextInt(50), new Random().nextInt(50), new Random().nextInt(2000), new Random().nextInt(2000), "15", "Android", "123:33:45:333"));
+    		count++;
+		}
 
-	            }
-	    });
+		
+		EventFeeder.pushToCEP(processes);
 	    
-	   
-	    
-		
-		
-		
-		
-		
-		
-		
-	}
+	 }
 	
 }
